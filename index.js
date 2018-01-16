9b6ae2932a7547b17e1a6d6f84f630ba40232727
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-16 04:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  delete object.key;
+  return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
